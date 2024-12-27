0ea9c6a47762e731a09e1328e6e50f5f1c2e9742
ControlNet <smczx@hotmail.com>
2024-12-28 05:16:41 +1100

    Update bwkWN3D3LScuYrjlJPjCgw.js

--- bwkWN3D3LScuYrjlJPjCgw.js
+++ bwkWN3D3LScuYrjlJPjCgw.js
@@ -1,3 +1,3 @@
-if (window.location.origin != "https://controlnet.space") {
+if (window.location.origin != "http://localhost:4000" && window.location.origin != "https://controlnet.space") {
     window.location.href = "https://controlnet.space";
 }
